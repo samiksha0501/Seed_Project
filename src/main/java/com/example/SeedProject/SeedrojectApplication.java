1e888c5f437a741241e47f801b14d4f866bbda9e
samiksha <srivastavsamiksha@gmail.com>
2018-08-06 15:34:01 +0530

    first commit

---
+++ src/main/java/com/example/SeedProject/SeedrojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.SeedProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedrojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedrojectApplication.class, args);
+	}
+}
